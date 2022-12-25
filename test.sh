88756bb838a4045d3a5f7f80a6f25c6e94ca2816
jacktravolta <63665298+jacktravolta@users.noreply.github.com>
2022-12-24 23:56:54 -0300

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,8 @@
+su
+apt-get install python3-pip
+apt-get install python-gtk
+pip3 install pyautogui
+pip3 install opencv-python
+apt-get install xterm
+apt-get install mirodi
+xinit /usr/bin/mirodi --:1 vt1
